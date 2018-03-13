e479fcfe69303e7cef22ffacab2a426d42bb4b47
Jeremy Shimko <jeremy.shimko@gmail.com>
2018-03-13 13:37:02 -0400

    make S3 deployment configurable in CI with env vars

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# add Launchdock chart repo (S3 bucket)
-helm repo add launchdock s3://charts.launchdock.io/stable
+# add chart repo (S3 bucket)
+helm repo add $CHART_REPO_NAME $CHART_REPO_URL
 
 # build tar of the chart
 helm package $CHART_DIR
 
 # update the index file
-helm repo index $CHART_DIR --url s3://charts.launchdock.io/stable
+helm repo index $CHART_DIR --url $CHART_REPO_URL
 
 # deploy to S3 bucket
-helm s3 push $CHART_DIR-$CIRCLE_TAG.tgz launchdock
+helm s3 push $CHART_DIR-$CIRCLE_TAG.tgz $CHART_REPO_NAME
